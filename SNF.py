fa14249e4047889b92192e2eda0eab685aa68a5e
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-04-24 13:24:35 +0200

    calculation of example

--- SNF.py
+++ SNF.py
@@ -1,4 +1,4 @@
 from sympy import Matrix, ZZ
 from sympy.matrices.normalforms import smith_normal_form
-m = Matrix([[12, 6, 4], [3, 9, 6], [2, 16, 14]])
+m = Matrix([[8, 2, 9,-2], [22, 2, 28, -8], [20, -6, 31, -12]])
 print(smith_normal_form(m, domain=ZZ))
