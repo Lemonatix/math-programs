85fa2d126717598140bc8876dcbfbddb66ee45a8
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-05-08 13:14:35 +0200

    calculates example for kgT

--- SNF.py
+++ SNF.py
@@ -1,4 +1,4 @@
 from sympy import Matrix, ZZ
 from sympy.matrices.normalforms import smith_normal_form
-m = Matrix([[8, 2, 9,-2], [22, 2, 28, -8], [20, -6, 31, -12]])
-print(smith_normal_form(m, domain=ZZ))
+m = Matrix([[-1, 0, 0], [0, -1, 0], [0, 1, -1]]) # ET für Matrix([[-1, 0, 0], [0, -1, 0], [0, 1, -1]]) ist 1
+print(smith_normal_form(m, domain=ZZ)) 
